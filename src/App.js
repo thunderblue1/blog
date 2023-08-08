a6d87c3fbec7de2aaf4f28c88636edecb876bf0c
John Keen <blueisgoodtoo@gmail.com>
2023-08-08 11:08:06 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Post from './Post';
+import AddPost from './AddPost';
 
 function App() {
+  const [postList, setPostList] = useState([
+    {
+      postNumber: 0,
+      text: 'A short psychic broke out of jail. She was a small medium at large.'
+    },
+    {
+      postNumber: 1,
+      text: 'More interesting reading if only I had more time ...',
+    },
+    { 
+      postNumber: 2,
+      text: 'Third blog post. I have run out of ideas.  Please comment below.'
+    }
+  ]);
+
+  const [postId, setPostId] = useState(3);
+
+  const handleAddPost = (newText) => {
+    let newPost = {
+      postNumber: postId,
+      text: newText
+    }
+
+    setPostList(postList => [...postList, newPost]);
+    setPostId(postId+1);
+  }
+
+  const handleDeletePost = (id) => {
+    let updatedPostList = postList.filter(post => post.postNumber !== id);
+    setPostList(updatedPostList);
+  }
+
+  const posts = postList.map((post) => {
+    return <Post key={post.postNumber} text={post.text} id={post.postNumber} onDelete={handleDeletePost}/>
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts}
+      <AddPost  onAdd={handleAddPost} />
     </div>
   );
 }
